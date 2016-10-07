727efe0e508f6f30ed40e187ddf6ceb66adb6136
Mateus Jabour <mjabourbp@gmail.com>
2016-10-07 18:28:16 -0400

    Set primary key type as an uuid and updates generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -1,4 +1,6 @@
 Rails.application.config.generators do |g|
+  g.orm :active_record, primary_key_type: :uuid
+  g.test_framework :test_unit, fixture: false
   g.assets = false
   g.helper = false
   g.skip_routes = true
