46c88157cb641a5e8b76672f6a07515a2917a3f8
Mateus Jabour <mjabourbp@gmail.com>
2017-06-14 20:48:01 -0400

    Adds properties to the user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 
   has_attached_file :avatar, styles: { small: "150x150>", thumb: "38x38>" }, default_url: "/assets/missing_avatar_:style.jpg"
 
-  validates_presence_of :username
+  validates_presence_of :username, :gender
   validates_email_format_of :email
   validates_uniqueness_of :email, :username
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
--- db/migrate/20161007223142_create_users.rb
+++ db/migrate/20161007223142_create_users.rb
@@ -7,9 +7,15 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.citext :username, null: false
       t.string :password_digest, null: false
       t.citext :email, null: false
+
+      t.string :biography
+			t.string :website
+      t.string :phone_number
+      t.string :gender, null: false
+
       t.boolean :is_private?, null: false, default: false
 
-      t.timestamps null:false
+      t.timestamps null: false
     end
 
     add_index :users, :email, unique: true
