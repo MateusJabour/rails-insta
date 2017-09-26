df00c8f4fe4e7978f90161f03d95c1b216d4d220
Mateus Jabour <mjabourbp@gmail.com>
2017-09-25 21:21:22 -0400

    Adds front end to handle user privacy.

--- app/assets/javascripts/controllers/relationship_controller.js
+++ app/assets/javascripts/controllers/relationship_controller.js
@@ -3,32 +3,41 @@
 
   var RelationshipController = (function () {
     function loadEvents() {
-      $(document).on('click', '.relationship-control', function (e) {
-          e.preventDefault();
-          e.stopPropagation();
+      $(document).on('click', '.pending-relationship-control', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
 
-          var relationshipButton = this;
-          var relationshipForm = this.parentNode;
-
-          $.ajax({
-            url: relationshipForm.action,
-            type: relationshipForm.method,
-            data: $(relationshipForm).serializeArray(),
-            dataType: 'json',
-            success: function(data) {
-              var newName = $(relationshipButton).data('inverse');
-              var newAction = $(relationshipButton).data('inverse-action');
-
-              $(relationshipButton).data('inverse', $(relationshipButton).val());
-              $(relationshipButton).data('inverse-action', relationshipForm.action);
-
-              relationshipForm.action = newAction;
-              $(relationshipButton).val(newName);
-
-              $('.user-profile__follower-counter').text(data);
-            }
-          });
+        $.ajax({
+          url: this.href,
+          type: 'POST',
+          data: [{name: 'relationship_id', value: $(this).data('relationshipId')}],
+          dataType: 'json',
+          success: function() {
+            $('.pending-relationship-control').remove();
+          }
         });
+      });
+
+      $(document).on('click', '.relationship-control', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var relationshipButton = this;
+        var relationshipForm = this.parentNode;
+
+        $.ajax({
+          url: relationshipForm.action,
+          type: relationshipForm.method,
+          data: $(relationshipForm).serializeArray(),
+          dataType: 'json',
+          success: function(data) {
+            relationshipForm.action = data.button_action;
+            $(relationshipButton).val(data.button_name);
+
+            $('.user-profile__follower-counter').text(data.follower_amount);
+          }
+        });
+      });
     }
 
     return ({
--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,5 +1,6 @@
 class NotificationController < ApplicationController
   def index
     @notifications = Notification.all.where(:notified_user => current_user.id)
+    @pending_followeds = current_user.pending_follower_relationships
   end
 end
